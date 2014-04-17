234ea1d805d2a02f16dbfc019931843364cc04b9
Miguel Martinez de Aguirre <miguel@aguirredevelopment.co.uk>
2014-04-17 23:09:47 +0100

    Changed to follow PEP8 with the help of SublimeLinter and flake8. Tab-indenting changed to space-indenting means most lines changed. Version previous to this was the first.

--- error.py
+++ error.py
@@ -4,19 +4,20 @@
 from twisted.cred import error
 from twisted.spread import pb
 
+
 class NameTaken(error.LoginFailed):
-	"""
-	Names are unique. This exception is thrown when a user requests
-	a name which is already taken.
-	"""
-	pass
+    """
+    Names are unique. This exception is thrown when a user requests
+    a name which is already taken.
+    """
+    pass
 
 
 class VersionMismatch(error.LoginFailed):
-	"""Client version is not recognised by this server."""
-	pass
+    """Client version is not recognised by this server."""
+    pass
 
 
 class IllegalNodeExpansion(pb.Error):
-	"""Attempted node expansion not allowed."""
-	pass
+    """Attempted node expansion not allowed."""
+    pass
--- util.py
+++ util.py
@@ -5,29 +5,29 @@ from itertools import product
 
 from twisted.python import log
 
+
 class ChildMaker(object):
-	def __init__(self, size, diagonals):
-		"""
-		size: (width, height) of the image
-		diagonals: boolean indicating whether diagonals are allowed
-		"""
-		self.size = size
-		self.diagonals = diagonals
+    def __init__(self, size, diagonals):
+        """
+        size: (width, height) of the image
+        diagonals: boolean indicating whether diagonals are allowed
+        """
+        self.size = size
+        self.diagonals = diagonals
 
-	def getChildren(self, node):
-		"""
-		node: (x, y) coordinates of parent for children
-		returns: set of viable children
-		"""
-		log.msg(["getChildren", self, node])
-		children = set()
-		if not self.diagonals:
-			vectors = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-		else:
-			vectors = [x for x in product((-1, 0, 1), (-1, 0, 1)) if x != (0, 0)]
-		for v in vectors:
-			child = (node[0] + v[0], node[1] + v[1])
-			if (0 <= child[0] < self.size[0]) and (0 <= child[1] < self.size[1]):
-				children.add(child)
-		return children
-
+    def getChildren(self, node):
+        """
+        node: (x, y) coordinates of parent for children
+        returns: set of viable children
+        """
+        log.msg(["getChildren", self, node])
+        children = set()
+        if not self.diagonals:
+            vectors = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        else:
+            vectors = [x for x in product((-1, 0, 1), (-1, 0, 1)) if x != (0, 0)]
+        for v in vectors:
+            child = (node[0] + v[0], node[1] + v[1])
+            if (0 <= child[0] < self.size[0]) and (0 <= child[1] < self.size[1]):
+                children.add(child)
+        return children
